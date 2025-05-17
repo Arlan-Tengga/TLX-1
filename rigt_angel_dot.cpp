813bdbc055f306155ec751c4ffc9e0168e8ebcf7
Arlan Tengga <arlantengga8@gmail.com>
2025-05-17 20:49:40 +0700

    first commit

---
+++ rigt_angel_dot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N = 10;
+    for(int i = 1;i<=N;i++){
+        for(int j = 1;j<=i;j++){
+            if(i==1||i==N){
+                cout<<"*";
+            }
+            else{
+                if (j==1||j==N)
+                {
+                    cout<<"*";
+                }
+                else{
+                    cout<<".";
+                }
+            }
+        }
+        cout<<endl;
+    }
+}
